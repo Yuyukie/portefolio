984c6ba0a8bbe6497c11de94c8b510086554a870
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-05-29 18:33:03 +0200

    pages project finie

--- pages/projects/grimoire.tsx
+++ pages/projects/grimoire.tsx
@@ -2,13 +2,36 @@ import Footer from "@/components/Footer";
 import ImageWithModal from "@/components/ImageWithModal";
 import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
+import Image from "next/image";
+import Link from "next/link";
 
 import React, { useState } from "react";
 
 const Grimoire = () => {
   const [nav, setnav] = useState(false);
+  const [showAll, setShowAll] = useState(false);
+
   const openNav = () => setnav(true);
   const closeNav = () => setnav(false);
+  const toggleShowAll = () => setShowAll(!showAll);
+
+  const images = [
+    "/images/obj.png",
+    "/images/scenario-1.png",
+    "/images/scenario-2.png",
+    "/images/spe-fct.png",
+    "/images/spe-fct-2.png",
+    "/images/spe-fct-3.png",
+    "/images/spe-API.png",
+    "/images/spe-API-2.png",
+    "/images/spe-API-3.png",
+    "/images/grimoire-1.png",
+    "/images/grimoire-2.png",
+    "/images/grimoire-3.png",
+    "/images/grimoire-4.png",
+  ];
+
+  const displayedImages = showAll ? images : images.slice(0, 3);
 
   return (
     <div className="flex flex-col overflow-x-hidden">
@@ -22,7 +45,7 @@ const Grimoire = () => {
             Presen
             <span className="text-yellow-400">tation</span>
           </h1>
-          <p className="mt-[2rem] text=[18px] text-slate-400 ">
+          <p className="mt-[2rem] text-[18px] text-slate-400">
             I have been a freelance back-end developer for a year now in the
             Lille region. I am used to working with Kévin, a front-end developer
             more experienced than you, and who has already a good network of
@@ -32,70 +55,48 @@ const Grimoire = () => {
             chain of bookstores that he wants open a book referencing and rating
             site: Mon Vieux Grimoire.
           </p>
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] mb-[4rem] gap-[3rem]  mx-auto">
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <ImageWithModal src="/images/obj.png" alt="" />
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/scenario-1.png" alt="" />
-              </div>
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/scenario-2.png" alt="" />
-              </div>
-            </article>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] mb-[4rem] gap-[3rem] mx-auto">
+            {displayedImages.map((src, index) => (
+              <article
+                key={index}
+                className="border-2 border-gray-400 bg-gray-400 hover:scale-105 transform transition-all duration-300"
+              >
+                <ImageWithModal src={src} alt="" />
+              </article>
+            ))}
           </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] mb-[4rem] gap-[3rem]  mx-auto">
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <ImageWithModal src="/images/spe-fct.png" alt="" />
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/spe-fct-2.png  " alt="" />
-              </div>
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/spe-fct-3.png" alt="" />
-              </div>
-            </article>
+          <div className="flex justify-center items-center gap-4">
+            <div className=" w-[6rem] h-[6rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
+              <Link
+                href="https://github.com/Yuyukie/Projet6"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/images/giticon.png"
+                  alt="123"
+                  width={50}
+                  height={50}
+                  className="md:w-[80px] md:h-[80px]"
+                />
+              </Link>
+            </div>
+            <div>
+              <h2 className="text-[25px] mb-[0.2rem] font-semibold text-white">
+                GitHub
+              </h2>
+            </div>
           </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] mb-[4rem] gap-[3rem]  mx-auto">
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <ImageWithModal src="/images/spe-API.png" alt="" />
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/spe-API-2.png" alt="" />
-              </div>
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/spe-API-3.png" alt="" />
-              </div>
-            </article>
+          <div className="mt-4 mb-4 text-white font-bold">
+            {`${displayedImages.length}/${images.length}`}
           </div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] mb-[4rem] gap-[3rem]  mx-auto">
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <ImageWithModal src="/images/grimoire-1.png" alt="" />
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/grimoire-2.png" alt="" />
-              </div>
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/grimoire-3.png" alt="" />
-              </div>
-            </article>
-            <article className="border-2 border-gray-400 bg-gray-400">
-              <div className="object-cover">
-                <ImageWithModal src="/images/grimoire-4.png" alt="" />
-              </div>
-            </article>
+          <div>
+            <button
+              onClick={toggleShowAll}
+              className=" mb-4 px-4 py-2 bg-yellow-400 text-white font-bold rounded"
+            >
+              {showAll ? "Fermer tous" : "Afficher plus"}
+            </button>
           </div>
         </div>
         <div>
@@ -105,4 +106,5 @@ const Grimoire = () => {
     </div>
   );
 };
+
 export default Grimoire;
