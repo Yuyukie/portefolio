bccf80af749357af58ad683625757450a6f76e8c
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-05-30 21:13:03 +0200

    push navbar fermeture auqnd click

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,27 +15,27 @@ const MobileNav = ({ nav, closeNav }: Props) => {
       className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}
     >
       <div className="w-[100vw] h-[100vh] flex flex-col md:hidden items-center justify-center">
-        <div className="nav-link-mobile">
+        <div className="nav-link-mobile" onClick={closeNav}>
           <Link href="/#home">Home</Link>
         </div>
-        <div className="nav-link-mobile">
+        <div className="nav-link-mobile" onClick={closeNav}>
           <Link href="/#services">Services</Link>
         </div>
-        <div className="nav-link-mobile">
+        <div className="nav-link-mobile" onClick={closeNav}>
           <Link href="/#about">About</Link>
         </div>
-        <div className="nav-link-mobile">
+        <div className="nav-link-mobile" onClick={closeNav}>
           <Link href="/#projects">Projects</Link>
         </div>
-        <div className="nav-link-mobile">
+        <div className="nav-link-mobile" onClick={closeNav}>
           <Link href="/#blog">Blog</Link>
         </div>
-        <div className="nav-link-mobile">
+        <div className="nav-link-mobile" onClick={closeNav}>
           <Link href="/#contact">Contact</Link>
         </div>
       </div>
       <div onClick={closeNav}>
-        <XMarkIcon className=" absolute z-[1000000] cursor-pointer top-[2.5rem] right-[4.5rem] w-[2rem] h-[2rem] text-yellow-400" />
+        <XMarkIcon className="absolute z-[1000000] cursor-pointer top-[2.5rem] right-[4.5rem] w-[2rem] h-[2rem] text-yellow-400" />
       </div>
     </div>
   );
