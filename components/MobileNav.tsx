e1535e13aadcc844f7b43294a21772680a50e038
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-05-30 21:10:29 +0200

    push navlink mobile

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,22 +15,22 @@ const MobileNav = ({ nav, closeNav }: Props) => {
       className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}
     >
       <div className="w-[100vw] h-[100vh] flex flex-col md:hidden items-center justify-center">
-        <div className="nav-link">
+        <div className="nav-link-mobile">
           <Link href="/#home">Home</Link>
         </div>
-        <div className="nav-link">
+        <div className="nav-link-mobile">
           <Link href="/#services">Services</Link>
         </div>
-        <div className="nav-link">
+        <div className="nav-link-mobile">
           <Link href="/#about">About</Link>
         </div>
-        <div className="nav-link">
+        <div className="nav-link-mobile">
           <Link href="/#projects">Projects</Link>
         </div>
-        <div className="nav-link">
+        <div className="nav-link-mobile">
           <Link href="/#blog">Blog</Link>
         </div>
-        <div className="nav-link">
+        <div className="nav-link-mobile">
           <Link href="/#contact">Contact</Link>
         </div>
       </div>
